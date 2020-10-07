295f115bbfdf7490053d18e49fa9a8c46284ed5c
Gina Sprint <sprint@gonzaga.edu>
2020-10-07 10:24:59 -0700

    end of wednesdays class

--- TimerFunS1/ViewController.swift
+++ TimerFunS1/ViewController.swift
@@ -19,27 +19,45 @@ class ViewController: UIViewController {
     // call invalidate() to stop a repeating timer
     var timer: Timer? = nil
     // task: add a seconds property (Int) with a property observer to update the secondsLabel
+    var seconds: Int = 0 {
+        didSet {
+            secondsLabel.text = "\(seconds)"
+        }
+    }
     // task: invalidate and set timer to nil
     // on pause and reset (something else)
     func startTimer() {
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer) in
             print("tick")
-            //seconds += 1
-            
+            self.seconds += 1 // fully qualify external refernce names
+            // recall: that closures "capture" their external references
+            // closures execute LATER
+            // perhaps when a variable that they refer to has been deallocated
+            // to prevent this issue, the closure "captures" the references and keeps them in memory/scope
         })
+    }
+    
+    func stopTimer() {
+        timer?.invalidate() // optional chaining
+        timer = nil
     }
     
     @IBAction func startPressed(_ sender: UIButton) {
         print("hello from start")
-        startTimer()
+        if timer == nil {
+            startTimer()
+        }
     }
     
     @IBAction func pausePressed(_ sender: UIButton) {
         print("hello from pause")
+        stopTimer()
     }
     
     @IBAction func resetPressed(_ sender: UIButton) {
         print("hello from reset")
+        stopTimer()
+        seconds = 0
     }
     
     override func viewDidLoad() {
